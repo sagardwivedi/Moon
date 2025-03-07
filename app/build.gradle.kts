becc7ad775bc4a5fd0cde62b70052ceb26245944
sagardwivedi <sagar.dw20@gmail.com>
2025-03-07 12:52:12 +0530

    Navigation dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
